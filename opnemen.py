e16ba71c8b1892c55185f14ede8dab7b09dc069b
agenshadow <162588121+agenshadow@users.noreply.github.com>
2024-03-29 20:59:55 +0100

    Add files via upload

---
+++ opnemen.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 19 08:48:08 2024
+
+@author: thijs
+"""
+import pyaudio
+import wave
+import time
+
+# Audio file specifications
+chunk = 1024
+format = pyaudio.paInt16
+rate = 44100
+seconds = 5
+channels = 1
+pause = 3
+
+# Start recording
+for i in range(3):
+    p = pyaudio.PyAudio()
+    stream = p.open(format=format,
+                    channels=channels,
+                    rate=rate,
+                    input=True,
+                    frames_per_buffer=chunk)
+    print("Start recording")
+    frames = []
+    for j in range(0, int(rate / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+    print("Recording stopped")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    # Convert recording to WAV file
+    wf = wave.open("meting{}_1.wav".format(i), 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(format))
+    wf.setframerate(rate)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    
+    print(pause, "s pauze")
+    time.sleep(pause)
+    print("volgende meting")
